111ae628745526de044915aade8173ec83733fc0
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2025-06-24 20:47:18 +0100

    chore: update pnpm workspace to include packages directory, add tsconfig.json for TypeScript configuration, and modify turbo.json outputs to include dist directory

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService, Languages } from './app.service';
+import { ApiResponse } from '@repo/types';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAvailableLanguages(): ApiResponse<Languages[]> {
+    return {
+      status: true,
+      statusCode: 200,
+      message: 'Available languages retrieved successfully',
+      data: this.appService.availableLanguages(),
+    };
   }
 }
--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Languages {
+  createdAt: Date;
+  language: string;
+  greet: string;
+}
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private languages: Languages[] = [
+    {
+      createdAt: new Date(),
+      language: 'English',
+      greet: 'Hello',
+    },
+    {
+      createdAt: new Date(),
+      language: 'French',
+      greet: 'Bonjour',
+    },
+    {
+      createdAt: new Date(),
+      language: 'Spanish',
+      greet: 'Hola',
+    },
+    {
+      createdAt: new Date(),
+      language: 'German',
+      greet: 'Hallo',
+    },
+    {
+      createdAt: new Date(),
+      language: 'Swahili',
+      greet: 'Habari',
+    },
+  ];
+  availableLanguages() {
+    return this.languages;
   }
 }
